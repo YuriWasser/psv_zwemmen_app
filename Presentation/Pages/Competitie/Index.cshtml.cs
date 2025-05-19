a3f765ac153cd07e14987114cea9cd5df56805c7
YuriWasser <yuri.wasser@live.nl>
2025-05-19 09:18:03 +0200

    changes

--- Presentation/Pages/Competitie/Index.cshtml.cs
+++ Presentation/Pages/Competitie/Index.cshtml.cs
@@ -9,24 +9,16 @@ namespace Presentation.Pages.Competitie;
 // Razor Page model voor het tonen van een lijst competities
 public class CompetitieModel : PageModel
 {
-    // Services voor competitie- en zwembadlogica
     private readonly CompetitieService _competitieService;
     private readonly ZwembadService _zwembadService;
 
-    // Constructor met dependency injection van de services
     public CompetitieModel(CompetitieService competitieService, ZwembadService zwembadService)
     {
         _zwembadService = zwembadService;
         _competitieService = competitieService;
     }
-
-    // Lijst van competities die aan de View worden getoond
     public List<CompetitieViewModel> Competities { get; set; } = new List<CompetitieViewModel>();
-
-    // Lijst van zwembaden 
     public List<Zwembad> Zwembaden { get; set; } = new List<Zwembad>();
-
-    // Methode die wordt aangeroepen bij een GET-verzoek naar de pagina
     public IActionResult OnGet()
     {
         List<Core.Domain.Competitie> competitites = _competitieService.GetAll();
